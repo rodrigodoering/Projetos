a25663db1dc00384c7f27e50ea68293b892e9ed6
rodrigodoering <34097975+rodrigodoering@users.noreply.github.com>
2021-05-12 20:30:28 -0300

    Update _utils.py
    Nesse script é implementada uma classe base (Superclasse) para a classe Graph.
    
    Nesse objeto devem ser implementadas as funções auxiliares da classe Graph, ou seja, funções que não interagem diretamente com o objeto Axes mas desempenham outras funções auxiliares ou complementares que podem ser acessadas pela Subclasse Graph

--- DataViz/_utils.py
+++ DataViz/_utils.py
@@ -5,92 +5,95 @@ Created on Sun Apr  4 11:03:02 2021
 @author: rodrigo doering neves
 @email: rodrigodoeringneves@gmail.com
 """
-
-# Importa módulos built-ins
-import warnings
-
-# Impede mensagens de aviso de serem exibidas no notebook
-warnings.filterwarnings("ignore")
-
-# Importa pacotes gerais da comunidade
 import pandas as pd
 import numpy as np
+import seaborn as sb
+import functools
+
 import matplotlib.pyplot as plt
+from matplotlib.axes import _subplots
 
-# Importa funções específicas
-from sklearn.metrics import accuracy_score
-from IPython.display import display, Markdown
+# User defined errors
+import _exceptions
 
+from typing import NoReturn
+from typing import Optional
+from typing import Iterator 
+from typing import Callable
+from typing import Generator 
+from typing import Iterable
+from typing import Union
+from typing import List
+from typing import Any
+from typing import Sequence
 
-# FUNÇÃO: numpy_convert 
-# Função para testar (e converter se necessário) listas de python em arrays numpy
-def numpy_convert(array):
-    '''
-    Argumentos:
-    ----------
-    array - lista de valores a ser validada
-    '''
-    if isinstance(array, list):
-        array = np.array(array)
-    return array
-
-
-# FUNÇÃO: number_to_string 
-# Converte um array em uma string compatível com a linguagem Latex
-def number_to_string(array):
-    '''
-    Argumentos:
-    ----------
-    array: numpy.array contendo os vetores que será convertido em string
-    '''      
-    rounded = (round(x,4) for x in array)
-    if array.dtype == np.float64:
-        # retorna as duas primeiras casas decimais se forem valores decimais
-        return ('%.2f' % val if str(val)[-2:] != '.0' else '%d' % val for val in rounded)
-    else:
-        return map(str, rounded)
+Numeric = Union[int, float, complex]
+NumericArray = Union[List[Numeric], np.ndarray, np.matrix]
 
 
-def display_expression(expr):
-    '''
-    Argumentos:
-    ----------
-    expr: string - expressão em LaTex para ser exibida
-    ''' 
-    if not isinstance(expr, str):
-        raise('Passe apenas strings')
-    display(Markdown(expr))
 
+class Graph_Utils:
     
-# FUNÇÃO: generate_grid_coords
-# Gera um grid de coordenadas para plotar uma visualização. Suporta receber uma função para computar
-# a última dimensão em função das outras anteriores. Convencionalmente, utiliza-se a função numpy.meshgrid
-# para gerar esse grid, mas acredito ser mais direto utilizar diretamente o numpy broadcasting
+    def __init__(self, n_axis: int):
+        self.n_axis = n_axis
+        
 
-def generate_grid_coords(n_dims, min_val=-1, max_val=1, n_vals=5, dim_func=None, return_flat=False):
-    base_var = np.linspace(min_val, max_val, n_vals)
-    update_axis = lambda i: (slice(np.newaxis),) + (np.newaxis,) * i
-    # Cria o grid de valores, len(grid) = n_dims
-    grid = np.broadcast_arrays(*(base_var[update_axis(i)] for i in range(n_dims)))
+    def validate_sequence(self, _input_: Sequence[Any]):
+        if len(_input_) != self.n_axis:
+            raise _exceptions.AxisCoordinatesError
+
+
+    def built_spec_kwargs(self, args: dict = None) -> dict:
+        specs_keys = ['labels', 'fontsize', 'lims', 'elev', 'azim', 'title', 'grid', 'legend']
+        specs = {key:None for key in specs_keys}
+        for key in ['grid', 'legend']:
+            specs[key] = False
+        if args is not None:
+            for key in args.keys():
+                specs[key] = args[key]
+        return specs
     
-    # Se for passada uma função para a última dimensão
-    if dim_func is not None:
-        dim_grid_shape = tuple(n_vals for i in range(n_dims))
-        last_dim_grid = dim_func(np.array([vec.flatten() for vec in grid[:-1]]).T).reshape(dim_grid_shape)
-        grid[-1] = last_dim_grid
-        
-    if return_flat:
-        # retorna os valores do grid como rowvectors de shape (n_vals^n_dims x n_dims)
-        return np.array([vec.flatten() for vec in grid]).T
+                
+    def iter_params(
+            self,
+            X: NumericArray, 
+            Y: NumericArray, 
+            Z: NumericArray = None,
+        ) -> Iterator[NumericArray]:
+        if self.n_axis == 2:
+            for coord in (X, Y):
+                yield coord
+                
+        elif self.n_axis == 3 and Z is None:
+            raise _exceptions.MissingZError
+            
+        else:
+            for coord in (X, Y, Z):
+                yield coord
 
-    return grid
 
-
+    def generate_grid_coords(
+            self, 
+            min_val: Numeric = -1, 
+            max_val: Numeric = 1, 
+            n_vals: int = 5, 
+            dim_func: Callable = None, 
+            return_flat: bool = False
+        ) -> NumericArray:
         
+        base_var = np.linspace(min_val, max_val, n_vals)
+        update_axis = lambda i: (slice(np.newaxis),) + (np.newaxis,) * i
+        # Cria o grid de valores, len(grid) = n_dims
+        grid = np.broadcast_arrays(*(base_var[update_axis(i)] for i in range(self.n_axis)))
         
-        
-        
-        
-        
-        
-        
+        # Se for passada uma função para a última dimensão
+        if dim_func is not None:
+            dim_grid_shape = tuple(n_vals for i in range(self.n_axis))
+            last_dim_grid = dim_func(np.array([vec.flatten() for vec in grid[:-1]]).T).reshape(dim_grid_shape)
+            grid[-1] = last_dim_grid
+            
+        if return_flat:
+            # retorna os valores do grid como rowvectors de shape (n_vals^n_dims x n_dims)
+            return np.array([vec.flatten() for vec in grid]).T
+    
+        return grid
